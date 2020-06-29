a448b79b21de90d3c1d1ee9f4546da591fc17649
JonTDean <JonathanTomDean@Gmail.com>
2020-06-29 06:54:59 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(aoh){
+  //Each property has a Year and a Result
+  let x = aoh.find( game => isWin(game));
+
+	if (x){
+		return x.year
+	}else{
+		return undefined
+	}
+}
+
+function isWin(game){
+  if(game.result == "W"){
+    return game.result
+  }else{
+    return undefined
+  }
+}
